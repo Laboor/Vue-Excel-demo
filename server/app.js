3ef0e502be33a88af0aa8192d446592e6d369449
Avalon <972003502@qq.com>
2020-02-17 01:10:36 +0800

    'build'

--- server/app.js
+++ server/app.js
@@ -5,13 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var boot = require('./boot');
 var responseBody = require('./middleware/responseBody');
+var history = require('connect-history-api-fallback');
 require('./global/Error');
-
-var createData = require('./createData');
 
 var cityInfoApi = require('./api/cityInfo');
 var branchInfoApi = require('./api/branchInfo');
-var transInfoApi = require('./api/transInfo');
 var upload = require('./api/upload');
 var exportFile = require('./api/export');
 
@@ -32,38 +30,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(history({
+  index: 'app/index.html'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(responseBody);
 
 app.use('/api/cityInfo', cityInfoApi);
 app.use('/api/branchInfo', branchInfoApi);
-app.use('/api/transInfo', transInfoApi);
 app.use('/api/upload', upload);
 app.use('/api/export', exportFile);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500); 
+  res.status(err.status || 500);
   res.send(err);
 });
 
 // Startup settings
 boot();
-
-// setTimeout(() => {
-//   createData();
-//   console.log('create data compelet!');
-// }, 3000);
-
 
 module.exports = app;
--- server/boot.js
+++ server/boot.js
@@ -4,7 +4,6 @@ const mongodb = require('./lib/mongodb');
 /* Methods loaded when the service starts */
 function boot() {
   mongodb.init(); // initialization MongoDB
-  // mongodb.connect("mongodb://localhost:27017/test"); // connect MongoDB
   mongodb.connect(process.env.DB_CONNECT_URL); // connect MongoDB
 }
 
